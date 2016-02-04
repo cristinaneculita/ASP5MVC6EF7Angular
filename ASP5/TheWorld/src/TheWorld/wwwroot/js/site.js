12ec78f4d881823a99e374530a529a0d092bf0f9
c.neculita <c.neculita@levi9.com>
2016-02-04 14:11:15 +0200

    1) HTML and CSS basics 2) javascript

---
+++ ASP5/TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿//site.js
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("Cristina & Alexandra");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function() {
+    //    main.style = "background-color: #888";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style = "background-color: #888";
+    //});
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text(" Show sidebar");
+        } else {
+            $(this).text(" Hide sidebar");
+        }
+    });
+
+})();
