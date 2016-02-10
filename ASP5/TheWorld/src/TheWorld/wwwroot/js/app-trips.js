4ea780723dbfd555accc12c1e5d9b5ce5ccca398
c.neculita <c.neculita@levi9.com>
2016-02-10 17:06:40 +0200

    AngularJS Routing

--- ASP5/TheWorld/src/TheWorld/wwwroot/js/app-trips.js
+++ ASP5/TheWorld/src/TheWorld/wwwroot/js/app-trips.js
@@ -2,5 +2,18 @@
     "use strict";
 
     //creating the module due to the second parameter
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+    .config(function($routeProvider) {
+            $routeProvider.when("/", {
+                controller: "tripsController",
+                controllerAs: "vm",
+                templateUrl:"/views/tripsView.html"
+            });
+            $routeProvider.when("/editor/:tripName", {
+                controller: "tripEditorController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripEditorView.html"
+            });
+            $routeProvider.otherwise({ redirectTo: "/" });
+        });
 })();
