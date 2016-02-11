780dd7f0fa7145d3a7ebe9c41d3c37c229f92c17
c.neculita <c.neculita@levi9.com>
2016-02-11 13:35:09 +0200

    ASP.NET 5 Deployment

--- ASP5/TheWorld/src/TheWorld/wwwroot/js/tripEditorController.js
+++ ASP5/TheWorld/src/TheWorld/wwwroot/js/tripEditorController.js
@@ -54,7 +54,7 @@
             travelMap.createMap({
                 stops: mapStops,
                 selector: "#map",
-                currentStop: 1,
+                currentStop: 0,
                 initialZoom: 3
             })
         }
